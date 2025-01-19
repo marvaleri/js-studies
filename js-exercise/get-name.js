67d173864b0045c9008d29f7c5e4265a7f03da50
Marlon <marlonsoares154@gmail.com>
2025-01-19 12:46:48 -0300

    feat(exercise): new folder about exercises, new html and js files

---
+++ js-exercise/get-name.js
@@ -0,0 +1,9 @@
+alert("Ola seja bem vindo! Por favor insira alguns dados")
+
+let nome = prompt("Qual é o seu nome?")
+
+let age = prompt("Qual é a sua idade?")
+
+var ageConfirm = confirm("Você tem certeza que sua idade é " + age + "?")
+
+alert("Nome: " +  nome + "\nIdade: " + age + "\nIdade confirmada: " + ageConfirm)
