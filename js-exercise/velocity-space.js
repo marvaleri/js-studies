ea43551bb569fdef59a9e125df0d46064babc57c
Marlon <marlonsoares154@gmail.com>
2025-01-21 22:07:21 -0300

    create new file of exercise, conditional and if else

---
+++ js-exercise/velocity-space.js
@@ -0,0 +1,25 @@
+let pilotName = prompt("Qual seu nome piloto?");
+
+let velocity = 0;
+
+let newVelocity = prompt("Gostaria de acelerar a nave a qual velocidade?");
+
+let confirmVelocity = confirm("Deseja confirmar a velocidade " + newVelocity + " km/s?");
+
+if (confirmVelocity){
+    velocity = newVelocity;
+}
+
+if (velocity <= 0 ){
+    alert("Nave está parada. Considere partir e acelerar");
+} else if (velocity < 40){
+    alert("Você está devagar. Podemos aumentar mais");
+} else if (velocity < 80){
+    alert("Parece uma boa velocidade para manter");
+} else if (velocity < 100){
+    alert("Velocidade alta. Considere diminuir");
+} else {
+    alert("Velocidade perigosa. Controle automático forçado");
+}
+
+alert("Piloto: " + pilotName + "\nVelocidade: " + velocity + " km/s");
